937d96f251c9617c248e4c1d00d20d3f0d9720ef
jung.andreas <jung.andreas@bluewin.ch>
2018-03-21 14:18:51 +0100

    load of solutions

--- app/controllers/BookController.java
+++ app/controllers/BookController.java
@@ -1,9 +1,15 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import models.Book;
+import play.libs.Json;
+import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
+import services.BookService;
+import services.DefaultBookService;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -12,33 +18,51 @@ import play.mvc.Result;
 @Api(value = "Book Controller", produces = "application/json")
 public class BookController extends Controller {
 
+    private final BookService bookService;
+
+    public BookController() {
+        bookService = new DefaultBookService();
+    }
+
     @ApiOperation(value = "Get Books", notes = "Get list of books.")
     public Result books(String q) {
-        return ok("Books"+ q);
+        return ok(Json.toJson(bookService.get()));
     }
 
+    // This way, a 400 HTTP response will be automatically returned for non JSON requests with Content-type set to application/json.
+    @BodyParser.Of(BodyParser.Json.class)
     public Result create() {
-        return ok("Create books");
+        final JsonNode json = request().body().asJson();
+        final Book book = Json.fromJson(json, Book.class);
+        return ok(Json.toJson(bookService.add(book)));
     }
 
+    @BodyParser.Of(BodyParser.Json.class)
     public Result update(Long id) {
-        if(id == 5) {
-            return notFound("Book with given id not found");
+        final JsonNode json = request().body().asJson();
+        final Book book = Json.fromJson(json, Book.class);
+        book.setId(id);
+        final Book updatedBook = bookService.update(book);
+        if(null != updatedBook) {
+            return ok(Json.toJson(updatedBook));
         }
-        return ok("Update book with id " + id);
+        return internalServerError();
+
     }
 
     public Result details(Long id) {
-        if(id == 5) {
-            return notFound("Book with given id not found");
+        Book book = bookService.get(id);
+        if(null == book) {
+            return notFound();
         }
-        return ok("Book details for book with id " + id);
+        return ok(Json.toJson(book));
     }
 
     public Result delete(Long id) {
-        if(id == 5) {
-            return notFound("Book with given id not found");
+        if(bookService.delete(id)) {
+            return ok();
         }
-        return ok("Delete book with id " + id);
+        return internalServerError();
     }
+
 }
